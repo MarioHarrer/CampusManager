70a8dc22bed2a4304956444872ec7a7f7736b71b
LucaGruber1 <lucaegger08@gmx.at>
2026-01-16 11:47:07 +0100

    database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,27 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { CalendarModule } from './calendar/calendar.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Auth } from './auth/entities/auth.entity';
+import { User } from './users/entities/user.entity';
+import { Calendar } from './calendar/entities/calendar.entity';
+
 
 @Module({
-  imports: [AuthModule, UsersModule, CalendarModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'admin',
+    password: 'supersecret',
+    database: 'todo_app',
+    entities: [Auth, User, Calendar],
+    synchronize: true,
+  }),
+    TypeOrmModule.forFeature([Calendar]),
+    AuthModule,
+    UsersModule,
+    CalendarModule],
   controllers: [AppController],
   providers: [AppService],
 })
